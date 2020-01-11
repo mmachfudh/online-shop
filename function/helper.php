1bb7ab1eadead970ffc938e60f506fa16de2a4d4
Mochammad Machfudh <machfudh@student.ub.ac.id>
2020-01-12 06:57:01 +0700

    form editing barang

--- function/helper.php
+++ function/helper.php
@@ -2,4 +2,8 @@
 
 	define("BASE_URL","http://localhost:8080/online-shop/");
 
+	function rupiah($nilai){
+		return "Rp ".number_format($nilai,2,",",".");
+	}
+
 ?>
--- module/kategori/form.php
+++ module/kategori/form.php
@@ -27,8 +27,9 @@
 	<div class="element-form">
 		<label>Status</label>
 		<span>
-			<input type="radio" name="status" value="on" <?php if($status=="on"){echo "checked='true'";}?>>On</span>
-			<input type="radio" name="status" value="off" <?php if($status=="off"){echo "checked='true'";}?> >Off</span>
+			<input type="radio" name="status" value="on" <?php if($status=="on"){echo "checked='true'";}?>/>On
+			<input type="radio" name="status" value="off" <?php if($status=="off"){echo "checked='true'";}?>/>Off 
+		</span>
 	</div>
 	
 	<div class="element-form">
